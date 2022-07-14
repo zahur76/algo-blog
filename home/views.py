010a56bd22939ae4adcb25694fc16754f13b5aee
zahur <zahurmeerun@hotmail.com>
2022-07-14 12:38:05 +0400

    setup: email comfiguration for comments

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
+from django.core.mail import send_mail
 
 
 # Create your views here
@@ -6,3 +7,27 @@ def index(request):
     """A view to return the index page"""
 
     return render(request, "home/index.html")
+
+
+def send_comment(request):
+    """ A send comment to mail """
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        comment = request.POST['comment']
+        print(username)
+
+        print(email)
+        print(comment)
+
+        subject = "Comment for SimpleAlgo"
+        body = comment
+
+        send_mail(
+            subject,
+            body,
+            email,
+            ["zahurmeerun@hotmail.com"]
+        )
+
+    return redirect(reverse("home"))
