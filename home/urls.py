5f00beb5b87705c616de12d54fc7a2e71c18bb5b
zahur <zahurmeerun@hotmail.com>
2022-07-14 19:02:00 +0400

    correct: typo's in literature

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="home"),
-    path("/send_comment", views.send_comment, name="send_comment"),
+    path("send_comment", views.send_comment, name="send_comment"),
 ]
