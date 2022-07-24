f0c0eb1cccef607faf5a05e6b5c2590af4852d9c
zahur <zahurmeerun@hotmail.com>
2022-07-24 14:09:55 +0400

    add: site ro admin panel

--- algo_blog/settings.py
+++ algo_blog/settings.py
@@ -42,12 +42,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'django.contrib.sitemaps',
 
     'fontawesomefree',
     'home',
     'maze'
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
