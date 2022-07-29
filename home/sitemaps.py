aa50e821d6ca24ea0fd7c2d139de1381d0b6ced2
zahur <zahurmeerun@hotmail.com>
2022-07-29 08:07:46 +0400

    add: to underground description

--- home/sitemaps.py
+++ home/sitemaps.py
@@ -2,6 +2,6 @@ from django.contrib.sitemaps import Sitemap
 from django.shortcuts import reverse
 class StaticViewSitemap(Sitemap):
     def items(self):
-        return ['home', 'maze']
+        return ['home', 'maze', 'part_two']
     def location(self, item):
         return reverse(item)
