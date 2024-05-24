9a1cb5a586be2f72b92f7f7ccff07da84e6a452a
zahur <zahurmeerun@hotmail.com>
2024-05-24 11:55:19 +0400

    add: explanation for MEDIA_URL

--- algo_blog/settings.py
+++ algo_blog/settings.py
@@ -137,8 +137,9 @@ STATIC_URL = 'static/'
 # For main static file not tied up to app and in base_dir/Required for base.css so django will look here too
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
-# This will add the file to the media folder and not to another url
+# This will provide url to serve media files
 MEDIA_URL = "/media/"
+# This will add the file to the media folder and not to another url
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
